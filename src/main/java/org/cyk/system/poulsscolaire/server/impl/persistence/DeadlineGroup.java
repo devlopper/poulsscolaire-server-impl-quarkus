ac14ffd6d012ed84b0f8000be5cf04454f51c6cb
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2024-08-06 19:29:53 +0000

    refactoring

--- src/main/java/org/cyk/system/poulsscolaire/server/impl/persistence/DeadlineGroup.java
+++ src/main/java/org/cyk/system/poulsscolaire/server/impl/persistence/DeadlineGroup.java
@@ -1,8 +1,14 @@
 package org.cyk.system.poulsscolaire.server.impl.persistence;
 
+import ci.gouv.dgbf.extension.server.persistence.entity.AbstractIdentifiable;
+import ci.gouv.dgbf.extension.server.persistence.entity.AbstractIdentifiableCodable;
+import ci.gouv.dgbf.extension.server.persistence.entity.AbstractIdentifiableCodableNamable;
 import ci.gouv.dgbf.extension.server.persistence.entity.AbstractIdentifiableCodableNamableAuditable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import org.hibernate.envers.AuditOverride;
+import org.hibernate.envers.AuditOverrides;
+import org.hibernate.envers.Audited;
 
 /**
  * Cette classe représente un groupe d'échéance.
@@ -12,6 +18,12 @@ import jakarta.persistence.Table;
  */
 @Entity(name = DeadlineGroup.ENTITY_NAME)
 @Table(name = DeadlineGroup.TABLE_NAME)
+@Audited
+@AuditOverrides(
+    value = {@AuditOverride(forClass = AbstractIdentifiableCodableNamableAuditable.class),
+        @AuditOverride(forClass = AbstractIdentifiableCodableNamable.class),
+        @AuditOverride(forClass = AbstractIdentifiableCodable.class),
+        @AuditOverride(forClass = AbstractIdentifiable.class)})
 public class DeadlineGroup extends AbstractIdentifiableCodableNamableAuditable {
 
   public static final String ENTITY_NAME = "DeadlineGroup";
