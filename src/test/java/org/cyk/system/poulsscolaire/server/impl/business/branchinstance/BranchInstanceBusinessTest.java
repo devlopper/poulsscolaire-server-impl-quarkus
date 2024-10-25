ba00a792dc686ba92b9db2846b18d982a4ee5e10
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2024-10-25 16:59:34 +0000

    refactoring

--- src/test/java/org/cyk/system/poulsscolaire/server/impl/business/branchinstance/BranchInstanceBusinessTest.java
+++ src/test/java/org/cyk/system/poulsscolaire/server/impl/business/branchinstance/BranchInstanceBusinessTest.java
@@ -1,8 +1,12 @@
 package org.cyk.system.poulsscolaire.server.impl.business.branchinstance;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import ci.gouv.dgbf.extension.core.Core;
+import ci.gouv.dgbf.extension.server.persistence.query.DynamicQueryParameters;
 import ci.gouv.dgbf.extension.server.service.api.request.GetManyRequestDto;
 import ci.gouv.dgbf.extension.test.AbstractTest;
 import io.quarkus.test.junit.QuarkusTest;
@@ -10,10 +14,15 @@ import io.quarkus.test.junit.QuarkusTestProfile;
 import io.quarkus.test.junit.TestProfile;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import java.util.List;
 import java.util.Map;
 import org.cyk.system.poulsscolaire.server.api.configuration.BranchInstanceDto;
+import org.cyk.system.poulsscolaire.server.api.configuration.BranchInstanceFilter;
 import org.cyk.system.poulsscolaire.server.impl.persistence.BranchInstance;
+import org.cyk.system.poulsscolaire.server.impl.persistence.BranchInstanceDynamicQuery;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @QuarkusTest
 @TestProfile(BranchInstanceBusinessTest.Profile.class)
@@ -34,11 +43,16 @@ class BranchInstanceBusinessTest extends AbstractTest {
   @Inject
   BranchInstanceMapper mapper;
   
+  @Inject
+  BranchInstanceDynamicQuery dynamicQuery;
+
+  DynamicQueryParameters<BranchInstance> dynamicQueryParameters = new DynamicQueryParameters<>();
+  
   @Test
   void readMany() {
     GetManyRequestDto request = new GetManyRequestDto();
     request.setAuditWho("christian");
-    assertEquals(5, readManyBusiness.process(request).getCount());
+    assertEquals(8, readManyBusiness.process(request).getCount());
   }
 
   @Test
@@ -65,6 +79,42 @@ class BranchInstanceBusinessTest extends AbstractTest {
     dto.setIdentifier("1");
     BranchInstance instance = mapper.mapFromDto(dto);
     assertEquals(dto.getIdentifier(), instance.getIdentifier());
+  }
+  
+  @ParameterizedTest
+  @CsvSource(value = {"1,1,1", "2,2,3:4:5", "3,3,"})
+  void getMany_whenSchoolIdentifierWhenBranchIdentifier(String schoolIdentifier,
+      String branchIdentifier, String expected) {
+    BranchInstanceFilter filter = new BranchInstanceFilter();
+    filter.setSchoolIdentifier(schoolIdentifier);
+    filter.setBranchIdentifier(branchIdentifier);
+    dynamicQueryParameters.setFilter(filter.toDto());
+    dynamicQueryParameters.projection().addNames(BranchInstanceDto.JSON_IDENTIFIER);
+    List<BranchInstance> instances = dynamicQuery.getMany(dynamicQueryParameters);
+    assertNotNull(instances);
+    if (Core.isStringBlank(expected)) {
+      assertEquals(0, instances.size());
+    } else {
+      assertLinesMatch(List.of(expected.split(":")),
+          instances.stream().map(i -> i.getIdentifier()).toList());
+    }
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {"1,6", "2,7:8", "3,"})
+  void getMany_whenSchoolingIdentifier(String schoolingIdentifier, String expected) {
+    BranchInstanceFilter filter = new BranchInstanceFilter();
+    filter.setSchoolingIdentifier(schoolingIdentifier);
+    dynamicQueryParameters.setFilter(filter.toDto());
+    dynamicQueryParameters.projection().addNames(BranchInstanceDto.JSON_IDENTIFIER);
+    List<BranchInstance> instances = dynamicQuery.getMany(dynamicQueryParameters);
+    assertNotNull(instances);
+    if (Core.isStringBlank(expected)) {
+      assertEquals(0, instances.size());
+    } else {
+      assertLinesMatch(List.of(expected.split(":")),
+          instances.stream().map(i -> i.getIdentifier()).toList());
+    }
   }
   
   public static class Profile implements QuarkusTestProfile {
